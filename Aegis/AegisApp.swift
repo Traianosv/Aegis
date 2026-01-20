926d587611356612d561dd247c1b0a981d44465b
TraianosVasileiou <109472391+Traianosv@users.noreply.github.com>
2026-01-20 20:35:39 +0200

    Initial Commit

---
+++ Aegis/AegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AegisApp.swift
+//  Aegis
+//
+//  Created by Τραϊανός  Βασιλείου on 20/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
